4994e17d7f2a99f978d1620473f1d759d381a413
A <andr25@email.ua>
2022-09-22 20:25:50 +0300

    main program

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from platform import python_version
 from flask import Flask, request
 
 app = Flask(__name__)
+my_num = 24
 
 
 def get_variant(student_id):
@@ -14,14 +15,13 @@ def get_variant(student_id):
     print("Current Python Version-", python_version())
 
 
-@app.route('/api/v1/hello-world-24', methods=['GET'])
+@app.route(f'/api/v1/hello-world-{my_num}', methods=['GET'])
 def helloworld():
     if request.method == 'GET':
         # data = {"data": "Hello World"}
         # return jsonify(data)
-        return "Hello World 24", 200
+        return f"Hello World {my_num}", 200
 
 
 if __name__ == '__main__':
-    my_num = 24
     app.run(debug=False)
